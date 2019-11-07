524f2496aa49f557018bfff9044e953dc346cf7e
Reginaldo Morikawa <morikawa77@gmail.com>
2019-11-07 09:26:47 -0300

    first commit

---
+++ Item.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace Console_OO
+{
+    class Item
+    {
+        public string NomeItem;
+        public double ValorItem;
+        public int QuantidadeItem;
+
+        public double AtualizaInventario()
+        {
+            return ValorItem * QuantidadeItem;
+        }
+
+        public void AdicionaItens(int quantidade)
+        {
+            QuantidadeItem += quantidade;
+        }
+
+        public void RemoveItens(int quantidade)
+        {
+            QuantidadeItem -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return NomeItem
+                + ", $ "
+                + ValorItem.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + QuantidadeItem
+                + " unidades, Total: $ "
+                + AtualizaInventario().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+using System.Globalization;
+
+namespace Console_OO
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Item i = new Item();
+            WriteLine("Entre com os dados do item: ");
+            Write("Nome do item: ");
+            i.NomeItem = ReadLine();
+            Write("Valor do item: ");
+            i.ValorItem = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+            Write("Quantidade no inventário: ");
+            i.QuantidadeItem = int.Parse(ReadLine());
+
+            WriteLine();
+            WriteLine("Dados do item: " + i);
+
+            WriteLine("");
+            Write("Digite o número de itens a ser adicionado ao estoque: ");
+            int qtdA = int.Parse(ReadLine());
+            i.AdicionaItens(qtdA);
+
+            WriteLine("");
+            WriteLine("Dados Atualizados: " + i);
+
+            WriteLine("");
+            Write("Digite o número de itens a ser removido do inventário: ");
+            int qtdR = int.Parse(ReadLine());
+            i.RemoveItens(qtdR);
+
+            WriteLine("");
+            WriteLine("Dados atualizados: " + i);
+        }
+    }
+}
